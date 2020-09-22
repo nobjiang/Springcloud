8ab182e7b8eb79fa47cf820d2dfc31a7efb2f83b
zhaoliang <170736012@qq.com>
2020-09-22 11:48:46 +0800

    hystrix

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/hystrix/order80/OrderHystrixMain80.java
@@ -0,0 +1,18 @@
+package com.hystrix.order80;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableHystrix
+@EnableCircuitBreaker
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class,args);
+    }
+}
+ 
---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/hystrix/order80/PaymentFallbackService.java
@@ -0,0 +1,16 @@
+package com.hystrix.order80;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PaymentFallbackService implements PaymentHystrixService {
+    @Override
+    public String paymentInfo_OK(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_OK , (┬＿┬)";
+    }
+
+    @Override
+    public String paymentInfo_TimeOut(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_TimeOut , (┬＿┬)";
+    }
+}
